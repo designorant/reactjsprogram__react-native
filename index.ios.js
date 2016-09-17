6f2f03c169f08526d687541bf130398d85b48319
Michał Ordon <michal@tehidea.com>
2016-09-17 01:04:12 +0100

    Clean up pesky semicolons

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
-import ReactModoro from './app';
-import { AppRegistry } from 'react-native';
+import ReactModoro from './app'
+import { AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent('ReactModoro', () => ReactModoro);
+AppRegistry.registerComponent('ReactModoro', () => ReactModoro)
