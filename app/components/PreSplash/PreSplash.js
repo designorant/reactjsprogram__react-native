dd5053ee96815c0328be390e432c442fefa11cbb
Michał Ordon <michal@tehidea.com>
2016-09-19 22:49:33 +0100

    Add PreSplash

---
+++ app/components/PreSplash/PreSplash.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+
+export default class PreSplash extends Component {
+  static propTypes = {}
+  state = {}
+  render () {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require('../../images/logo.png')} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    resizeMode: 'contain',
+    height: 300
+  }
+})
