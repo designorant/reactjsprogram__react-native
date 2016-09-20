18fecad79438090c192153b2bd483440d364ce2b
Michał Ordon <michal@tehidea.com>
2016-09-20 22:10:45 +0100

    Animate PreSplash

--- app/components/PreSplash/PreSplash.js
+++ app/components/PreSplash/PreSplash.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Animated } from 'react-native'
 
 export default class PreSplash extends Component {
   static propTypes = {}
-  state = {}
+  state = {
+    rotation: new Animated.Value(0)
+  }
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      Animated.sequence([
+        Animated.timing(this.state.rotation, {toValue: -1, duration: 150}),
+        Animated.timing(this.state.rotation, {toValue: 1, duration: 150}),
+        Animated.timing(this.state.rotation, {toValue: 0, duration: 250})
+      ]).start()
+    }, 1000)
+  }
+  componentWillUnmount () {
+    window.clearInterval(this.interval)
+  }
+  getTransform () {
+    return {
+      transform: [
+        {
+          rotate: this.state.rotation.interpolate({
+            inputRange: [-1, 1],
+            outputRange: ['-20deg', '20deg']
+          })
+        }
+      ]
+    }
+  }
   render () {
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.image}
+        <Animated.Image
+          style={[styles.image, this.getTransform()]}
           source={require('../../images/logo.png')} />
       </View>
     )
