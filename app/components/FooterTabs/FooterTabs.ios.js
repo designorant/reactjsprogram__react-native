278099d99465ea7e9e8da43a90c30ade940ddd59
Michał Ordon <michal@tehidea.com>
2016-09-21 22:52:10 +0100

    Set up TabBarIOS in FooterTabs

--- app/components/FooterTabs/FooterTabs.ios.js
+++ app/components/FooterTabs/FooterTabs.ios.js
@@ -1,20 +1,34 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { TabBarIOS, Text } from 'react-native'
+import { colors } from '~/styles'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 FooterTabs.propTypes = {
-
+  activeFooterTab: PropTypes.string.isRequired,
+  navigator: PropTypes.object.isRequired,
+  onTabSelect: PropTypes.func.isRequired
 }
 
 export default function FooterTabs (props) {
+  console.log('Here!', props)
   return (
-    <View>
-      <Text>
-        FooterTabs
-      </Text>
-    </View>
+    <TabBarIOS tintColor={colors.active}>
+      <Icon.TabBarItem
+        iconSize={35}
+        iconName='ios-home-outline'
+        title='Home'
+        selected={props.activeFooterTab === 'home'}
+        onPress={() => props.onTabSelect('home')}>
+        <Text>HOME</Text>
+      </Icon.TabBarItem>
+      <Icon.TabBarItem
+        iconSize={35}
+        iconName='ios-trophy-outline'
+        title='Leaderboard'
+        selected={props.activeFooterTab === 'leaderboard'}
+        onPress={() => props.onTabSelect('leaderboard')}>
+        <Text>LEADERBOARD</Text>
+      </Icon.TabBarItem>
+    </TabBarIOS>
   )
 }
-
-const styles = StyleSheet.create({
-
-})
