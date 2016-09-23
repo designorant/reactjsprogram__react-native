de84d9463cda0c50d26811c6c63f363926f333ae
Michał Ordon <michal@tehidea.com>
2016-09-24 00:52:05 +0100

    Prepare for firebase authentication

--- app/redux/modules/authentication.js
+++ app/redux/modules/authentication.js
@@ -1,3 +1,26 @@
+const AUTHENTICATING = 'AUTHENTICATING'
+const NOT_AUTHED = 'NOT_AUTHED'
+const IS_AUTHED = 'IS_AUTHED'
+
+function authenticating () {
+  return {
+    type: AUTHENTICATING
+  }
+}
+
+function notAuthed () {
+  return {
+    type: NOT_AUTHED
+  }
+}
+
+function isAuthed (uid) {
+  return {
+    type: IS_AUTHED,
+    uid
+  }
+}
+
 const initialState = {
   isAuthed: false,
   isAuthenticating: false,
@@ -6,6 +29,23 @@ const initialState = {
 
 export default function authentication (state = initialState, action) {
   switch (action.type) {
+    case AUTHENTICATING:
+      return {
+        ...state,
+        isAuthenticating: true
+      }
+    case NOT_AUTHED:
+      return {
+        isAuthed: false,
+        isAuthenticating: false,
+        authedId: ''
+      }
+    case IS_AUTHED:
+      return {
+        isAuthed: true,
+        isAuthenticating: false,
+        authedId: action.uid
+      }
     default:
       return state
   }
