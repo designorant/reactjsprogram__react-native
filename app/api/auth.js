87362ab9168a3ff2425b4c022136c4fd17cefff2
Michał Ordon <michal@tehidea.com>
2016-09-27 00:39:00 +0100

    Hook up firebase properly

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,4 +1,4 @@
-import { firebaseAuth, facebookProvider } from '~/config/constants'
+import { firebaseAuth, facebookProvider, ref } from '~/config/constants'
 import { AccessToken } from 'react-native-fbsdk'
 
 export function getAccessToken () {
@@ -7,4 +7,8 @@ export function getAccessToken () {
 
 export function authWithToken (accessToken) {
   return firebaseAuth.signInWithCredential(facebookProvider.credential(accessToken))
+}
+
+export function updateUser (user) {
+  return ref.child(`users/${user.uid}`).set(user)
 }
