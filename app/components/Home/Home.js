217128b789f6cf02747d5fcb47c7f2dcd5bb206c
Michał Ordon <michal@tehidea.com>
2016-09-30 17:32:12 +0100

    Hook up Hamburger icon

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -3,16 +3,16 @@ import { View, StyleSheet, Text, Platform } from 'react-native'
 import { ReactModoroNavbar, Gear, Hamburger } from '~/components'
 
 Home.propTypes = {
-
+  openDrawer: PropTypes.func
 }
 
 export default function Home (props) {
   return (
     <View>
       <ReactModoroNavbar
-        title='Foobar'
+        title='Home'
         rightButton={<Gear onPress={() => console.log('Gear!')} />}
-        leftButton={Platform.OS === 'android' ? <Hamburger onPress={() => console.log('Hamburger!')} /> : null}
+        leftButton={Platform.OS === 'android' ? <Hamburger onPress={props.openDrawer} /> : null}
       />
       <Text>
         Home
--- app/components/Leaderboard/Leaderboard.js
+++ app/components/Leaderboard/Leaderboard.js
@@ -1,20 +1,21 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Platform } from 'react-native'
+import { ReactModoroNavbar, Gear, Hamburger } from '~/components'
 
 Leaderboard.propTypes = {
-
+  openDrawer: PropTypes.func
 }
 
 export default function Leaderboard (props) {
   return (
     <View>
+      <ReactModoroNavbar
+        title='Leaderboard'
+        leftButton={Platform.OS === 'android' ? <Hamburger onPress={props.openDrawer} /> : null}
+      />
       <Text>
         Leaderboard
       </Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-
-})
