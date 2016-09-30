aeea4f5b176c7c7916b1bd897763dd9b92e4163b
Michał Ordon <michal@tehidea.com>
2016-09-30 17:19:50 +0100

    Add Hamburger on Android

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
-import { ReactModoroNavbar, Gear } from '~/components'
+import { View, StyleSheet, Text, Platform } from 'react-native'
+import { ReactModoroNavbar, Gear, Hamburger } from '~/components'
 
 Home.propTypes = {
 
@@ -12,6 +12,7 @@ export default function Home (props) {
       <ReactModoroNavbar
         title='Foobar'
         rightButton={<Gear onPress={() => console.log('Gear!')} />}
+        leftButton={Platform.OS === 'android' ? <Hamburger onPress={() => console.log('Hamburger!')} /> : null}
       />
       <Text>
         Home
--- app/components/Icons/Hamburger.js
+++ app/components/Icons/Hamburger.js
@@ -3,21 +3,21 @@ import { TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { colors } from '~/styles'
 
-Gear.propTypes = {
+Hamburger.propTypes = {
   size: PropTypes.number.isRequired,
   style: PropTypes.object,
   onPress: PropTypes.func.isRequired
 }
 
-Gear.defaultProps = {
+Hamburger.defaultProps = {
   size: 30
 }
 
-export default function Gear (props) {
+export default function Hamburger (props) {
   return (
     <TouchableOpacity onPress={props.onPress} style={props.style}>
       <Icon
-        name='ios-settings-outline'
+        name='ios-menu-outline'
         size={props.size}
         color={colors.blue} />
     </TouchableOpacity>
