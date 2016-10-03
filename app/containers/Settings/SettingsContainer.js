6faae8eb6572119f31f7240dd03d063ad14d76a0
Michał Ordon <michal@tehidea.com>
2016-10-03 23:42:40 +0100

    Add Settings UI

--- app/containers/Settings/SettingsContainer.js
+++ app/containers/Settings/SettingsContainer.js
@@ -6,10 +6,36 @@ export default class SettingsContainer extends Component {
   static propTypes = {
     navigator: PropTypes.object.isRequired
   }
-  state = {}
+  state = {
+    timerDuration: 20,
+    restDuration: 5
+  }
+  handleTimerChange = (timerDuration) => {
+    this.setState({timerDuration})
+  }
+  handleRestChange = (restDuration) => {
+    this.setState({restDuration})
+  }
+  handleTimerComplete = () => {
+    console.log('Finished Sliding Timer')
+  }
+  handleRestComplete = () => {
+    console.log('Finished Sliding Rest Timer')
+  }
+  handleLogout = () => {
+    console.log('Logging Out!')
+  }
   render () {
     return (
-      <Settings onBack={this.props.navigator.pop} />
+      <Settings
+        onBack={this.props.navigator.pop}
+        onTimerChange={this.handleTimerChange}
+        onRestChange={this.handleRestChange}
+        onTimerComplete={this.handleTimerComplete}
+        onRestComplete={this.handleRestComplete}
+        onLogout={this.handleLogout}
+        timerDuration={this.state.timerDuration}
+        restDuration={this.state.restDuration} />
     )
   }
 }
