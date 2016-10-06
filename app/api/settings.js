b7aa1ebefa9c925327a4bb91538c65e9183cb4fb
Michał Ordon <michal@tehidea.com>
2016-10-06 14:52:57 +0100

    Retrieve settings from Firebase

--- app/api/settings.js
+++ app/api/settings.js
@@ -9,3 +9,33 @@ export function setRest (duration, uid) {
   return ref.child(`settings/${uid}/restDuration`)
     .set(duration)
 }
+
+export function fetchSettings (uid) {
+  return ref.child(`settings/${uid}`)
+    .once('value')
+    .then((snapshot) => {
+      const timerDuration = 20
+      const restDuration = 5
+
+      const settings = snapshot.val()
+
+      if (settings === null) {
+        return {
+          timerDuration,
+          restDuration
+        }
+      } else if (!settings.timerDuration) {
+        return {
+          timerDuration,
+          restDuration: settings.restDuration
+        }
+      } else if (!settings.restDuration) {
+        return {
+          restDuration,
+          timerDuration: settings.timerDuration
+        }
+      } else {
+        return settings
+      }
+    })
+}
