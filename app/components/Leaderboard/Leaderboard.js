c1fcbc1426dae6194a1d8756d07f1c8925e2343a
Michał Ordon <michal@tehidea.com>
2016-10-18 23:53:51 +0100

    Refactor Leaderboard to use ListView

--- app/components/Leaderboard/Leaderboard.js
+++ app/components/Leaderboard/Leaderboard.js
@@ -1,33 +1,36 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Platform, ActivityIndicator } from 'react-native'
+import { View, StyleSheet, Platform, ActivityIndicator, ListView } from 'react-native'
 import { ReactModoroNavbar, Hamburger } from '~/components'
 import { colors } from '~/styles'
-import Leader from './Leader'
 
 Leaderboard.propTypes = {
   openDrawer: PropTypes.func,
   listenerSet: PropTypes.bool.isRequired,
-  leaders: PropTypes.array.isRequired
+  dataSource: PropTypes.object.isRequired,
+  renderRow: PropTypes.func.isRequired
 }
 
 export default function Leaderboard (props) {
   return (
-    <View>
+    <View style={styles.container}>
       <ReactModoroNavbar
         title='Leaderboard'
         leftButton={Platform.OS === 'android' ? <Hamburger onPress={props.openDrawer} /> : null}
       />
       {props.listenerSet === false
         ? <ActivityIndicator size='small' style={styles.activityIndicator} color={colors.secondary} />
-        : props.leaders.map((leader) => (
-          <Leader name={leader.displayName} avatar={leader.photoURL} score={leader.score} key={leader.uid} />
-        ))
+        : <ListView renderRow={props.renderRow} dataSource={props.dataSource} />
       }
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    marginBottom: 50
+  },
   activityIndicator: {
     marginTop: 30
   }
--- app/containers/Leaderboard/LeaderboardContainer.js
+++ app/containers/Leaderboard/LeaderboardContainer.js
@@ -1,7 +1,9 @@
 import React, { PropTypes, Component } from 'react'
+import { ListView } from 'react-native'
 import { Leaderboard } from '~/components'
 import { connect } from 'react-redux'
 import { fetchAndSetScoresListener } from '~/redux/modules/scores'
+import Leader from '~/components/Leaderboard/Leader'
 
 class LeaderboardContainer extends Component {
   static propTypes = {
@@ -9,6 +11,13 @@ class LeaderboardContainer extends Component {
     navigator: PropTypes.object.isRequired,
     listenerSet: PropTypes.bool.isRequired,
     leaders: PropTypes.array.isRequired
+  }
+  constructor (props) {
+    super(props)
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    this.state = {
+      dataSource: this.ds.cloneWithRows(this.props.leaders)
+    }
   }
   componentDidMount () {
     if (this.props.listenerSet === false) {
@@ -18,13 +27,20 @@ class LeaderboardContainer extends Component {
   componentWillReceiveProps (nextProps) {
     if (nextProps.leaders !== this.props.leaders) {
       console.log('New Leaders!', nextProps.leaders)
+      this.setState({
+        dataSource: this.ds.cloneWithRows(nextProps.leaders)
+      })
     }
+  }
+  renderRow = ({displayName, photoURL, score}) => {
+    return <Leader name={displayName} avatar={photoURL} score={score} />
   }
   render () {
     return (
       <Leaderboard
         listenerSet={this.props.listenerSet}
-        leaders={this.props.leaders}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
         openDrawer={this.props.openDrawer} />
     )
   }
